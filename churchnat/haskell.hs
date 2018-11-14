a094689933d7b3307b06f725d8ae8b0ae9e409a1
bivoje <bivoje49@gist.ac.kr>
2018-11-15 04:54:50 +0900

    Adopt unrolling type wrapper in haskell sol
    As suggested in some blog, I could use newtype to
    go around recursive types. but things got tricky as
    the system developed. I guess it's possible to keep
    doing so, while it's way much complex that I can\
    handle write now.
    
    It seems that i need untyped, lazy evaluated functional
    programming language. wikipedia page of
    'Comparison_of_functional_programming_languages'
    lists some of those. among them, only closure and lisp
    are qualified to be so. but both are too dirty when
    writing churchnat code.. maybe.. can I implement new
    language (haskell like - but untyped) that can represent
    untyped lambda calculus?

--- churchnat/haskell.hs
+++ churchnat/haskell.hs
@@ -1,59 +1,58 @@
 {-# LANGUAGE RankNTypes #-}
--- TODO https://wiki.haskell.org/Rank-N_types
 
-import Prelude hiding (succ, exp)
+import Prelude hiding (succ, exp, head, pred)
 import Data.Bool
 import Data.Either
 
 ---------------------------------
 -- Natural numbers
 
-type Nat = forall a . (a -> a) -> (a -> a)
+type Nat a = (a -> a) -> (a -> a)
 
-church2nat :: Nat -> Int
+church2nat :: Nat Int -> Int
 church2nat n = n (+1) 0
 
-nat2church :: Int -> Nat
+nat2church :: Int -> Nat a
 --nat2church n = \f x -> foldl (flip ($)) x $ replicate n f
 nat2church n f x = foldl (flip id) x (replicate n f)
 
-zero :: Nat
+zero :: Nat a
 zero f = id
 
-one :: Nat
+one :: Nat a
 one = id
 
-two :: Nat
+two :: Nat a
 two f = f . f
 
-three :: Nat
+three :: Nat a
 three f = f . f . f
 
-succ :: Nat -> Nat
+succ :: Nat a -> Nat a
 succ n f = f . n f
 -- succ = Monad.ap (.)
 
-four :: Nat
+four :: Nat a
 four = succ three
 
-plus :: Nat -> Nat -> Nat
+plus :: Nat a -> Nat a -> Nat a
 plus n m f = m f . n f
 -- plus = liftM2 (.)
 
-five :: Nat
+five :: Nat a
 five = plus two three
 
-mult :: Nat -> Nat -> Nat
+mult :: Nat a -> Nat a -> Nat a
 mult = (.)
 
-six :: Nat
+six :: Nat a
 six = mult two three
 
-exp :: Nat -> Nat -> Nat
+--exp :: Nat -> Nat -> Nat
 exp = flip ($)
 -- exp = flip id
 
-eight :: Nat
+eight :: Nat a
 eight = exp two three
 
 -- helped by http://pointfree.io/
@@ -61,40 +60,39 @@ eight = exp two three
 ---------------------------------
 -- Boolean
 
-type Boolean = forall a . a -> a -> a
+type Boolean a = a -> a -> a
 
---church2bool :: Boolean -> Bool
-church2bool :: (Bool -> Bool -> Bool) -> Bool
+church2bool :: Boolean Bool -> Bool
 church2bool f = f True False
 
-bool2church :: Bool -> Boolean
+bool2church :: Bool -> Boolean a
 --bool2church b = if b then true else false
 bool2church = bool false true
 
-true :: Boolean
+true :: Boolean a
 --true f g = f
 true = const
 
-false :: Boolean
+false :: Boolean a
 false f g = g
 -- false = const id
 
-neg :: Boolean -> Boolean
+neg :: Boolean a -> Boolean a
 neg = flip
 
-conj :: Boolean -> Boolean -> Boolean
+conj :: Boolean a -> Boolean a -> Boolean a
 conj b1 b2 = \x y -> b1 (b2 x y) y
 
-disj :: Boolean -> Boolean -> Boolean
+disj :: Boolean a -> Boolean a -> Boolean a
 disj b1 b2 = \x y -> b1 x (b2 x y)
 
-xand :: Boolean -> Boolean -> Boolean
+xand :: Boolean a -> Boolean a -> Boolean a
 xand b1 b2 = \x y -> b1 (b2 x y) (b2 y x)
 
-xorr :: Boolean -> Boolean -> Boolean
+xorr :: Boolean a -> Boolean a -> Boolean a
 xorr b1 b2 = \x y -> b1 (b2 y x) (b2 x y)
 
-booltbl2 :: (Boolean -> Boolean -> Boolean) -> IO ()
+booltbl2 :: (Boolean Bool -> Boolean Bool -> Boolean Bool) -> IO ()
 booltbl2 f = do
   print . church2bool $ f true true
   print . church2bool $ f true false
@@ -104,7 +102,7 @@ booltbl2 f = do
 ---------------------------------
 -- Tuple
 
-type Tuple a = Boolean -> a
+type Tuple a = Boolean a -> a
 
 church2tuple :: Tuple a -> (a, a)
 church2tuple t = (carT t, cdrT t)
@@ -124,7 +122,7 @@ cdrT t = t false
 ---------------------------------
 -- Pair
 
-type Pair a b = Boolean -> Either a b
+type Pair a b = Boolean (Either a b) -> Either a b
 
 church2pair :: Pair a b -> (a, b)
 church2pair p = (carP p, cdrP p)
@@ -141,30 +139,136 @@ cdrP p = fromRight undefined $ p false
 ---------------------------------
 -- List
 
-type List a = forall b . (b -> a -> b) -> b -> b
+type List b a = (b -> a -> b) -> b -> b
 
-church2list :: List a -> [a]
+church2list :: List [a] a -> [a]
 church2list ls = reverse $ ls (flip (:)) []
 
-list2church :: [a] -> List a
+list2church :: [a] -> List b a
 list2church ls = \f x -> foldl f x ls
 
-cons :: a -> List a -> List a
+cons :: a -> List b a -> List b a
 cons a ls = \f b -> ls f (f b a)
 
-empty :: List a
+empty :: List b a
 empty _ = id
 
-append :: List a -> List a -> List a
+append :: List b a -> List b a -> List b a
 append xs ys = \f x -> ys f (xs f x)
 
---head :: List a -> a
---head ls = cdrP $ ls (\b a ->
---    (carP b) (consP true (cdrP b)) (consP true a)
---  ) (consP false undefined)
+head ls = cdrP $ ls (\b a ->
+    (carP b) (consP true (cdrP b)) (consP true a)
+  ) (consP false undefined)
+
+{-
+> head :: List a -> a
+> head ls = cdrP $ ls (\b a ->
+>     (carP b) (consP true (cdrP b)) (consP true a)
+>   ) (consP false undefined)
+
+the head above doesn't work.
+head empty == undefined
+head $ cons 1 empty == 1
+to analyze, split it into 2 functions.
+
+> foldf b a = (carP b) (consP true (cdrP b)) (consP true a)
+> head ls = cdrP $ ls foldf (consP false undefined)
+
+I have guessed its type
+> Pair (Boolean x) a -> a -> Pair (Boolean x) a
+but it wasn't. the correct type is
+> Pair (Boolean (Pair (Boolean x) a)) a -> a -> Pair (Boolean x) a
+where `x` is replaced with (Pair (Bool x) a)).
+becuase (carP b) is a boolean that should be applied on
+`(consP true a)` which has type `Pair (Boolean x) a`, its type
+should be (Boolean (Pair (Boolean x) a)).
+
+you can see that `cdrP $ foldf (cons false undefined) 1` returns
+1 as expected.
+
+head can't be defined using foldf since List requires accumulator
+of type `b -> a -> b` which foldf can't satisfy. if haskell's get
+forced to type check head (or the kind of expression), it would
+make long -infinit- type chain. you can see it by running
+the following
+
+> true = const
+> false f g = g
+> consP x y = \b -> b (Left x) (Right y)
+> carP p = fromLeft undefined $ p true
+> cdrP p = fromRight undefined $ p false
+> empty = \_ b -> b
+> cons a ls = \f b -> ls f (f b a)
+> foldf b a = (carP b) (consP true (cdrP b)) (consP true a)
+> head ls = cdrP $ ls foldf (consP false undefined)
+>
+> head empty == undefined
+> head (cons 1 empty) == 1
+> head (cons 2 (cons 1 empty) == error!
+
+actually the same problem occures when defining y combinator.
+> y f = (\x -> f (x x)) (\x -> f (x x))
+above definition fails to typecheck.
+this is because haskell is typed lambda calculus.
+while above definition (and head) is written in untyped lambda calc.
+you can solve this problem by
+http://r6.ca/blog/20060919T084800Z.html
+
+I thought I defined head and s (in python) without using recursion.
+but in fact, they we implicitly using structur of the y combinator.
+-}
 
 ---------------------------------
--- HashMap
+-- Rec
+
+--b = b -> a
+newtype Mu a = Roll { unroll :: Mu a -> a }
+
+--Roll   :: (Mu a -> a) -> Mu a
+--unroll :: Mu a -> Mu a -> a
+
+--fix f = (\x -> f (x x)) (\x -> f (x x))
+fix f = (\x -> f . unroll x $ x) (Roll $ \x -> f . unroll x $ x)
+
+isZero :: Nat (Boolean a) -> Boolean a
+isZero n = n (const false) true
+
+--x = Pair (Boolean x) (Nat y)
+newtype Nu a b = Roll_Nu { unroll_nu :: Pair (Boolean (Nu a b)) (Nat b) }
+
+-- Roll_Nu   :: Pair (Boolean (Nu a b)) (Nat b) -> Nu a b
+-- unroll_Nu :: Nu a b -> Pair (Boolean (Nu a b)) (Nat b)
+
+--pred_rec :: Pair (Boolean (Pair (Boolean x) (Nat y))) (Nat y)
+--                        -> Pair (Boolean x) (Nat y)
+pred_rec :: Nu a b -> Nu a b
+pred_rec x = (carP $ unroll_nu x)
+  (Roll_Nu $ consP true (succ . cdrP $ unroll_nu x))
+  (Roll_Nu $ consP true zero)
+--pred_rec x = (carP x) (consP true . succ . cdrP $ x) (consP true zero)
+
+pred :: Nat (Nu a b) -> Nat b
+pred n = cdrP . unroll_nu $ n (pred_rec) (Roll_Nu $ consP false zero)
+
+--fact :: (Nat a -> Nat a) -> Nat a -> Nat a
+--fact f n = (isZero n) one (mult n (f (pred n)))
+--n2        :: Nat (Nu x a)
+--n1        :: Nat (Boolean (Nat a))
+--isZero n1 :: Nat (Boolean (Nat a)) -> Boolean (Nat a)
+--Nat a = Nat (Boolean (Nat a))
+--Ru a = Boolean (Nat a)
+--Ru a = Boolean (Nat (Ru a))
+
+newtype Ru a = Roll_Ru { unroll_ru :: Boolean (Nat (Ru a)) }
+-- Roll_Ru   :: Boolean (Nat (Roll_Ru a)) -> Ru a
+-- unroll_ru :: Ru a -> Boolean (Nat (Roll_Ru a))
+
+--fact f n = (isZero n) one (mult n (f (pred n)))
+--one       :: Nat (Ru a)
+--isZero n1 :: Nat (Boolean (Nat (Ru a))) -> Boolean (Nat (Ru a))
+--          :: Nat (Ru a) -> Boolean (Nat (Ru a))
+--n1        :: Nat (Boolean (Nat (Ru a)))
+--          :: Nat (Ru a)
 
 ---------------------------------
 -- Integer
